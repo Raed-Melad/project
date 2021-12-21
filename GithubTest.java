a62a6dda9149aa525396a4ea76dc57d7806712c9
AhmedEissa2002 <96464406+AhmedEissa2002@users.noreply.github.com>
2021-12-21 09:09:42 +0200

    Add files via upload

---
+++ GithubTest.java
@@ -0,0 +1,23 @@
+
+package githubtest;
+
+public class GithubTest {
+
+   
+    public static void main(String[] args) {
+       
+        int x = 9;
+        int y=1;
+        int z;
+        z=(x+y)/2;
+        System.out.println(z);
+        
+              
+        
+        
+        
+        
+        
+    }
+    
+}
